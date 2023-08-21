7082bd521387fecc1a39a6a869b6208099db4554
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-08-21 11:11:26 +0530

    build the starting screen of the app:
    - add OutlinedButton and style it.
    - add padding between the button, text and image using SizedBox.

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,36 @@ void main() {
           ),
           child: Center(
             child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
-                Image.asset('assets/images/quiz-logo.png'),
-                const Text('Learn Flutter the FUN Way!'),
+                Image.asset(
+                  'assets/images/quiz-logo.png',
+                  width: 350,
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                const Text(
+                  'Learn Flutter the FUN Way!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Start Quiz",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
               ],
             ),
           ),
