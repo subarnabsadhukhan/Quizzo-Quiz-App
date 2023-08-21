554d8a458912b3c501a6151b886fc74837036d3c
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-08-21 10:45:23 +0530

    build the basic structure of the app: - add MaterialApp Widget, inside it add Scaffold Widget - use gradient colors for background

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 71, 0, 145),
+                Color.fromARGB(255, 98, 0, 163),
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Column(
+              children: [
+                Text('Learn Flutter the FUN Way!'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
