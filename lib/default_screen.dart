c64058d760712629c1f9074ac61f0d335055c3c9
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-08-21 13:01:42 +0530

    split the code on a separate file by creating custom widget StartScreen():
    As this app has multiple screens, we need to design each screens separately for differnt content.
    The DefaultScreen() will render same Gradient Background on every Screen.
    
    - build StartScreen() Widget for the Starting Screen of the Quiz App.
    - transfer all the codes for Starting Screen to this widget.

--- lib/default_screen.dart
+++ lib/default_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
 
 class DefaultScreen extends StatelessWidget {
   const DefaultScreen({super.key});
@@ -15,41 +16,7 @@ class DefaultScreen extends StatelessWidget {
           ],
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/quiz-logo.png',
-              width: 350,
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            const Text(
-              'Learn Flutter the FUN Way!',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            OutlinedButton(
-              onPressed: () {},
-              child: const Text(
-                "Start Quiz",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
+      child: const StartScreen(),
     );
   }
 }
---
+++ lib/start_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Learn Flutter the FUN Way!',
+            style: TextStyle(
+              color: Color.fromARGB(255, 237, 223, 252),
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            child: const Text(
+              "Start Quiz",
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
