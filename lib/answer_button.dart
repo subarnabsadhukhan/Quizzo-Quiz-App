9f9ab693497ecb0f463d4a68f5aac151353f217f
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-08-22 18:22:18 +0530

    make a Reusable Button widget for option buttons:
    - create a AnswerButton Widget extending StatelessWidget.
    - AnswerButton Widget will take two named argument onTap and answerText.
    - style the ElevatedButton using built-in ElevatedButton.styleFrom() constructor function.

---
+++ lib/answer_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({
+    required this.answerText,
+    required this.onTap,
+    super.key,
+  });
+  final String answerText;
+  final void Function() onTap;
+  @override
+  Widget build(context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 40,
+          vertical: 10,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            40,
+          ),
+        ),
+        foregroundColor: Colors.white,
+        backgroundColor: const Color.fromARGB(
+          255,
+          36,
+          5,
+          98,
+        ),
+      ),
+      child: Text(
+        answerText,
+      ),
+    );
+  }
+}
