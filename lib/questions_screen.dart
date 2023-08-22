15f88407532bf242311234b588612bf8384ca8e8
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-08-22 21:17:31 +0530

    manage the questions index as State

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -11,9 +11,17 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex += 1;
+    });
+  }
+
   @override
   Widget build(context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -39,7 +47,7 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
               (e) {
                 return AnswerButton(
                   answerText: e,
-                  onTap: () {},
+                  onTap: answerQuestion,
                 );
               },
             ),
