708c68a3f22bd2118ec11174f27906a76a8187da
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-08-22 20:34:00 +0530

    add alignment and margin to questions and options

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -16,27 +16,35 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(
-              color: Colors.white,
+      child: Container(
+        margin: const EdgeInsets.all(
+          40,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 19,
+                color: Colors.white,
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          ...currentQuestion.answers.map(
-            (e) {
-              return AnswerButton(
-                answerText: e,
-                onTap: () {},
-              );
-            },
-          ),
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.answers.map(
+              (e) {
+                return AnswerButton(
+                  answerText: e,
+                  onTap: () {},
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
