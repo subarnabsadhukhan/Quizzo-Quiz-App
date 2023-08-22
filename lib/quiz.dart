8a9b2bd2628b5b80929653e1708815afc0defdda
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-08-22 07:32:54 +0530

    implement changing screen code using ternary expressions (version 2):
    - this version needs less code than version 1
    - need to lifting up state
    - create an activeScreen Widget and switchScreen function in quiz.dart
    - in start_screen.dart, create a positional argument startQuiz and pass it as a pointer to the button.
    - startQuiz is defined as final void Function(): void, because it does not return anything; Function, because onPressed accepts void Function; and empty (), because switchScreen() also don't take argument.

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
 class Quiz extends StatefulWidget {
@@ -10,6 +11,14 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
   @override
   Widget build(context) {
     return MaterialApp(
@@ -25,7 +34,9 @@ class _QuizState extends State<Quiz> {
               ],
             ),
           ),
-          child: const StartScreen(),
+          child: activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : const QuestionsScreen(),
         ),
       ),
     );
