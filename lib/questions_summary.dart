f397484d7607412d7c804116cf1ab4ce5e6f138a
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-08-23 18:30:50 +0530

    make QuestionsSummary Widget for result screen:
    - .toString() returns a String representative of an Object.
    - Here, as String is type casting.

---
+++ lib/questions_summary.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class QuestionsSummary extends StatelessWidget {
+  const QuestionsSummary(
+    this.summaryData, {
+    super.key,
+  });
+
+  final List<Map<String, Object>> summaryData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: summaryData.map(
+        (data) {
+          return Row(
+            children: [
+              Text(
+                ((data['question_index'] as int) + 1).toString(),
+              ),
+              Column(
+                children: [
+                  Text(
+                    (data['question']).toString(),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(data['user_answer'] as String),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    (data['correct_answer']).toString(),
+                  )
+                ],
+              )
+            ],
+          );
+        },
+      ).toList(),
+    );
+  }
+}
